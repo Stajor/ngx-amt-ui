aaf6923abd3f837885adaee937adbbf4910f5e58
Alex <alex.belenky@appendad.com>
2025-04-05 18:55:00 +0300

    Export interfaces

--- projects/amt-ui/src/public-api.ts
+++ projects/amt-ui/src/public-api.ts
@@ -3,7 +3,8 @@
  */
 export * from './lib/services/amt-theme.service';
 export * from './lib/services/amt-menu.service';
-export * from './lib/services/storage.service';
+export * from './lib/interfaces/amt-menu.interface';
+export * from './lib/interfaces/amt-theme.inteface';
 export * from './lib/auth-layout/auth-layout.component';
 export * from './lib/dashboard-layout/dashboard-layout.component';
 export * from './lib/errors/errors.module';
