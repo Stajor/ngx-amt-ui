ba1b0a8ace8fcbb0d3409bba0ed91c6599afac3e
Alex <alex.belenky@appendad.com>
2025-04-05 14:59:38 +0300

    Add navbar colors component

--- projects/amt-ui/src/lib/dashboard-layout/navbar-colors/navbar-colors.component.ts
+++ projects/amt-ui/src/lib/dashboard-layout/navbar-colors/navbar-colors.component.ts
@@ -19,13 +19,13 @@ export class NavbarColorsComponent {
   amtThemeService = inject(AmtThemeService);
 
   themeColors: {name: AmtThemeColor, code: string}[] = [
-    { name: 'base', code: '#e11d48' },
-    { name: 'yellow', code: '#f59e0b' },
-    { name: 'green', code: '#22c55e' },
-    { name: 'blue', code: '#3b82f6' },
-    { name: 'orange', code: '#ea580c' },
-    { name: 'red', code: '#cc0022' },
-    { name: 'violet', code: '#6d28d9' },
+    {name: 'base', code: '#e11d48'},
+    {name: 'yellow', code: '#f59e0b'},
+    {name: 'green', code: '#22c55e'},
+    {name: 'blue', code: '#3b82f6'},
+    {name: 'orange', code: '#ea580c'},
+    {name: 'red', code: '#cc0022'},
+    {name: 'violet', code: '#6d28d9'}
   ];
 
   toggleThemeColor(color: AmtThemeColor) {
