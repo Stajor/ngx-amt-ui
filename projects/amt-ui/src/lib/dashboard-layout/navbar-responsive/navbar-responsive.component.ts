00cedf4dc3f9165076746a2d5f70ceccbc77bd4d
Alex <alex.belenky@appendad.com>
2025-04-05 15:12:30 +0300

    Add navbar responsive component

---
+++ projects/amt-ui/src/lib/dashboard-layout/navbar-responsive/navbar-responsive.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amt-navbar-responsive',
+  imports: [],
+  templateUrl: './navbar-responsive.component.html',
+  styleUrl: './navbar-responsive.component.css'
+})
+export class NavbarResponsiveComponent {
+
+}
